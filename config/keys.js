aed6fcca281fd05e756eb22c37f67c38652d5b31
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-07-05 21:45:27 +0530

    update
    update

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword='mongodb+srv://khageswar9:Dellinspire123@mychatapp.ymdjs.mongodb.net/?retryWrites=true&w=majority'
-
-module.exports = {
-  mongoURI: dbPassword
-};
---
+++ index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require('./db');
+const express = require('express');
+var cors = require('cors')
+const app = express()
+
+connectToMongo();
+
+app.use(cors())
+app.use(express.json());
+//available routes
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/', require('./routes/jobs'))
+
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
+  
+app.listen(5000, ()=>{
+    console.log(`job portal listening at port 5000`)
+})
